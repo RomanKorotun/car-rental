f62d66e26655411ca3094b21c1789f8ff29629b1
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2024-02-25 14:06:17 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { App } from './components/App';
-import { GlobalStyle } from './GlobalStyle';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor } from './redux/store';
-import store from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./components/App";
+import { GlobalStyle } from "./GlobalStyle";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./redux/store";
+import store from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/car-rental">
           <App />
           <GlobalStyle />
         </BrowserRouter>
