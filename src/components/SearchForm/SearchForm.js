5c0b02da519d4d82aada1780140c61639814ff51
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2024-03-13 16:25:21 +0200

    fix filters

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -33,6 +33,7 @@ const validShema = Yup.object().shape({
 export const SearchForm = () => {
   const dispatch = useDispatch();
   const { brand, price, mileageFrom, mileageTo } = useCars();
+  console.log(mileageFrom);
 
   const handleClick = () => {
     dispatch(
@@ -62,7 +63,7 @@ export const SearchForm = () => {
         <LabelBrand>
           <BrandCard>
             <TitleBrand> Car brand</TitleBrand>
-            <FieldStyledBrand component="select" name="brand">
+            <FieldStyledBrand component="select" name="brand" value={brand}>
               <option value="all">All brands</option>
               <option value="buick">Buick</option>
               <option value="volvo">Volvo</option>
@@ -92,7 +93,7 @@ export const SearchForm = () => {
         <LabelPrice>
           <PriceCard>
             <TitlePrice>Price,$ / 1 hour</TitlePrice>
-            <FieldStyledPrice component="select" name="price">
+            <FieldStyledPrice component="select" name="price" value={price}>
               <option value="all">All prices</option>
               <option value="25">25</option>
               <option value="30">30</option>
@@ -129,88 +130,5 @@ export const SearchForm = () => {
         </BtnForm>
       </FormStyled>
     </Formik>
-    // <Formik
-    //   initialValues={{
-    //     brand: brand,
-    //     price: price,
-    //     mileageFrom: mileageFrom,
-    //     mileageTo: mileageTo,
-    //   }}
-    //   validationSchema={validShema}
-    //   onSubmit={(values) => {
-    //     dispatch(filters(values));
-    //   }}
-    // >
-    //   <FormStyled>
-    //     <LabelBrand>
-    //       <BrandCard>
-    //         <TitleBrand> Car brand</TitleBrand>
-    //         <FieldStyledBrand component="select" name="brand">
-    //           <option value="all">All brands</option>
-    //           <option value="buick">Buick</option>
-    //           <option value="volvo">Volvo</option>
-    //           <option value="hummer">Hummer</option>
-    //           <option value="subaru">Subaru</option>
-    //           <option value="mitsubishi">Mitsubishi</option>
-    //           <option value="nissan">Nissan</option>
-    //           <option value="lincoln">Lincoln</option>
-    //           <option value="gmc">GMC</option>
-    //           <option value="hyundai">Hyundai</option>
-    //           <option value="mini">MINI</option>
-    //           <option value="bentley">Bentley</option>
-    //           <option value="mercedes-benz">Mercedes-Benz</option>
-    //           <option value="aston martin">Aston Martin</option>
-    //           <option value="pontiac">Pontiac</option>
-    //           <option value="lamborghini">Lamborghini</option>
-    //           <option value="audi">Audi</option>
-    //           <option value="bmw">BMW</option>
-    //           <option value="chevrolet">Chevrolet</option>
-    //           <option value="chrysler">Chrysler</option>
-    //           <option value="kia">Kia</option>
-    //           <option value="land rover">Land Rover</option>
-    //         </FieldStyledBrand>
-    //       </BrandCard>
-    //     </LabelBrand>
-
-    //     <LabelPrice>
-    //       <PriceCard>
-    //         <TitlePrice>Price,$ / 1 hour</TitlePrice>
-    //         <FieldStyledPrice component="select" name="price">
-    //           <option value="all">All prices</option>
-    //           <option value="25">25</option>
-    //           <option value="30">30</option>
-    //           <option value="35">35</option>
-    //           <option value="40">40</option>
-    //           <option value="45">45</option>
-    //           <option value="50">50</option>
-    //           <option value="55">55</option>
-    //           <option value="75">75</option>
-    //           <option value="100">100</option>
-    //           <option value="150">150</option>
-    //           <option value="200">200</option>
-    //           <option value="250">250</option>
-    //           <option value="300">300</option>
-    //           <option value="500">500</option>
-    //         </FieldStyledPrice>
-    //       </PriceCard>
-    //     </LabelPrice>
-
-    //     <LabelStyledMileage>
-    //       <MileageCard>
-    //         <TitleMileage> Сar mileage / km</TitleMileage>
-    //         <div>
-    //           <FieldStyledMileageFrom type="string" name="mileageFrom" />
-    //           <FieldStyledMileageTo type="string" name="mileageTo" />
-    //         </div>
-    //       </MileageCard>
-    //       <AbsoluteFrom>From</AbsoluteFrom>
-    //       <AbsoluteTo>To</AbsoluteTo>
-    //     </LabelStyledMileage>
-    //     <BtnForm type="submit">Search</BtnForm>
-    //     <BtnForm onClick={handleClick} type="button">
-    //       Reset
-    //     </BtnForm>
-    //   </FormStyled>
-    // </Formik>
   );
 };
